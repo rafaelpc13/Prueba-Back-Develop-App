5d4ee2d3b46137699682c328bbd17c41bf1e238d
rafaelpc13 <59027749+rafaelpc13@users.noreply.github.com>
2024-03-15 12:38:38 -0500

    primer commit

---
+++ routes/productosRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const router = express.Router();
+import { InsertarProducto,ObtenerProductos} from "../controllers/productosController.js";
+
+//Creacion, registro  y confrimacion de usuarios
+
+router.post("/insertar", InsertarProducto);
+router.get("/obtener", ObtenerProductos);
+
+/* router.post("/insertar", InsertarUsuario);
+
+router.post("/login", autenticar);
+router.get("/confirmar/:token",confirmar);
+router.post('/olvide-password', olvidePassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+
+router.get("/perfil",checkAuth,perfil) */
+
+router.get('/', (req, res) => {
+    res.json('Desde API/USUARIOS');
+});
+
+export default router;
